cd4fc5278232ed5a3d0ca6bc0844860a4d66aeff
Titus Fortner <titusfortner@gmail.com>
2020-03-18 17:37:07 -0500

    add threadlocal for TestNG

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,25 +4,40 @@ import com.saucelabs.framework.Browser;
 import com.saucelabs.framework.pages.PageObject;
 import com.saucelabs.saucebindings.SauceOptions;
 import com.saucelabs.saucebindings.SauceSession;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
+
 public class BaseTest {
-    public SauceSession session;
+    protected static ThreadLocal<SauceSession> session = new ThreadLocal<>();
+    protected static ThreadLocal<SauceOptions> options = new ThreadLocal<>();
+
+    public SauceSession getSession() {
+        return session.get();
+    }
+
+    public WebDriver getDriver() {
+        return getSession().getDriver();
+    }
 
     @BeforeMethod
-    public void setup() {
-        SauceOptions options = new SauceOptions();
-        session = new SauceSession(options);
-        RemoteWebDriver driver = session.start();
+    public void setup (Method method) {
+        options.set(new SauceOptions());
+        options.get().setName(method.getName());
+        session.set(new SauceSession(options.get()));
+
+        RemoteWebDriver driver = getSession().start();
+
         Browser browser = new Browser(driver);
         PageObject.setBrowser(browser);
     }
 
     @AfterMethod
-    public void tearDown(ITestResult result) throws Exception {
-        session.stop(result.isSuccess());
+    public void tearDown(ITestResult result) {
+        getSession().stop(result.isSuccess());
     }
 }
